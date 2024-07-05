342c7334e5b96a408fcc713f0340bbd1896de8b5
unknown <ricardodonado1105@gmail.com>
2024-07-05 13:52:10 -0500

    menu animation created

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,15 @@ const navMenu = document.getElementsByClassName("menu")[0]
 const mainElement = document.getElementsByTagName("main")[0]
 
 navButton.addEventListener("click", ()=>{
+    headerElement.classList.toggle("opened")
     navMenu.classList.toggle("opened")
     mainElement.classList.toggle("opened")
-    headerElement.classList.toggle("opened")
+
+    headerElement.classList.toggle("closed")
+    if(navMenu.classList.contains("opened")){
+        navMenu.classList.remove("closed")
+    }else{
+        navMenu.classList.add("closed")
+    }
+    mainElement.classList.toggle("closed")
 })

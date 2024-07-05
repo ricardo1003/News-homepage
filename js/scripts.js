e9d74b49d619ccc8ce2938ffc9d2cca8a45589ec
unknown <ricardodonado1105@gmail.com>
2024-07-05 12:53:37 -0500

    mobile menu stylized

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("hello world!")
+const headerElement = document.getElementsByTagName("header")[0]
+const navButton = document.getElementsByClassName("open-close")[0]
+const navMenu = document.getElementsByClassName("menu")[0]
+const mainElement = document.getElementsByTagName("main")[0]
+
+navButton.addEventListener("click", ()=>{
+    navMenu.classList.toggle("opened")
+    mainElement.classList.toggle("opened")
+    headerElement.classList.toggle("opened")
+})
